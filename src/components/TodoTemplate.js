41d14f041c5b062a57d13cd7148177de82757061
sungminim <__so1@hanmail.net>
2021-03-31 14:02:11 +0900

    feature/todoTemplate

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -1,5 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
 
-function TodoTemplate(){
-    
+const TodoTemplateBlock = styled.div`
+    position: relative;
+    display:flex;
+    flex-direction: column;
+    width: 512px;
+    height: 768px;
+    margin:0 auto;
+    margin-top:96px;
+    margin-bottom:32px;
+    background: #fff;
+    border-radius:16px;
+    box-shadow:0 0 8px rgba(0,0,0,0.04)
+`
+
+function TodoTemplate({ children }){
+    return (
+        <TodoTemplateBlock>{ children }</TodoTemplateBlock>
+    )
 }
+
+export default TodoTemplate
